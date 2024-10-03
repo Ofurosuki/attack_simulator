e97feede88029993eef2ace9db51c4f2a12059a1
Ofurosuki <ryo-yoshida@keio.jp>
2024-10-03 13:03:49 +0900

    first commit

--- src/hfr_simulator.cpp
+++ src/hfr_simulator.cpp
@@ -1,5 +1,11 @@
+#include <Eigen/Dense>
+#include <fstream>
+#include <iostream>
+#include <random>
 #include <sensor_msgs/msg/point_cloud2.hpp>
 #include <sensor_msgs/point_cloud2_iterator.hpp>
+#include <sstream>
+#include <string>
 
 #include "rclcpp/rclcpp.hpp"
 
@@ -20,10 +26,96 @@ class HFRSimulator : public rclcpp::Node {
 
     this->declare_parameter("elimination_angle", 20.0);
     elimination_angle = this->get_parameter("elimination_angle").as_double();
+    readCSVtoEigen("/home/awsim/Downloads/success_rate_matrix.csv",
+                   success_rate_matrix);
   }
 
  private:
   float elimination_angle = 0;
+
+  float azimuth_range = 40.0;  // range of array corresponding to, degree
+  float alititude_range = 30.0;
+
+  float csv_row = 0;
+  float csv_column = 0;
+
+  float azimuth_step = 0;
+  float alititude_step = 0;
+
+  Eigen::MatrixXd success_rate_matrix;
+
+  void readCSVtoEigen(const std::string& filePath,
+                      Eigen::MatrixXd& outputMatrix) {
+    std::ifstream file(filePath);
+    if (!file.is_open()) {
+      RCLCPP_ERROR(this->get_logger(), "Error opening file!");
+      return;
+    }
+    RCLCPP_INFO(this->get_logger(), "File opened successfully!");
+
+    std::string line;
+    std::vector<std::vector<double>> data;
+
+    // Read each line from the CSV file
+    while (std::getline(file, line)) {
+      std::stringstream lineStream(line);
+      std::string cell;
+      std::vector<double> row;
+
+      // Read each cell in the line
+      while (std::getline(lineStream, cell, ',')) {
+        row.push_back(std::stod(cell));
+      }
+
+      data.push_back(row);
+    }
+
+    file.close();
+
+    // Determine row and column count
+    int rows = data.size();
+    int cols = data[0].size();
+    RCLCPP_INFO(this->get_logger(), "Rows: %d, Columns: %d", rows, cols);
+
+    outputMatrix.resize(rows, cols);
+    for (int i = 0; i < rows; ++i) {
+      for (int j = 0; j < cols; ++j) {
+        outputMatrix(i, j) = data[i][j];
+      }
+    }
+    csv_row = rows;
+    csv_column = cols;
+    azimuth_step = azimuth_range / rows;
+    alititude_step = alititude_range / cols;
+    // Return row and column count
+    return;
+  }
+
+  std::pair<int, int> get_index(const float azimuth, const float altitude) {
+    int i = (azimuth + azimuth_range / 2) / azimuth_step;
+    int j = (altitude + alititude_range / 2) / alititude_step;
+    return std::make_pair(i, j);
+  }
+  bool is_attack_successful(const float azimuth, const float altitude,
+                            const Eigen::MatrixXd& matrix) {
+    if (azimuth > azimuth_range / 2.0 || azimuth < -azimuth_range / 2.0 ||
+        altitude > alititude_range / 2.0 || altitude < -alititude_range / 2.0) {
+      return 0;
+    }
+    std::pair<int, int> index = get_index(azimuth, altitude);
+    RCLCPP_INFO(this->get_logger(), "index: %d, %d", index.first, index.second);
+    float success_rate = matrix(index.first, index.second);
+    // Generate a random number between 0 and 1
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(0.0, 1.0);
+    if (dis(gen) < success_rate) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
   void pointcloud_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg) {
     // // Make a copy of the message
     // auto updated_msg = std::make_shared<sensor_msgs::msg::PointCloud2>(*msg);
@@ -64,13 +156,18 @@ class HFRSimulator : public rclcpp::Node {
       cos_phi = x / sqrt(x * x + y * y);
 
       // if (x >= min_x && x <= max_x && y >= min_y && y <= max_y && z >= min_z
-      // && z <= max_z) if (x <= min_x || x >= max_x) if (!(y >= min_y && y <=
-      // max_y))
-      if (cos_phi <= cos(elimination_angle / 2 * M_PI / 180)) {
-        // Copy valid points to filtered data
+      // &&
+      //     z <= max_z)
+      //   if (x <= min_x || x >= max_x)
+      //     if (!(y >= min_y && y <= max_y))
+      if (cos_phi <= cos(azimuth_range / 2 * M_PI / 180)) return;
+      if (!is_attack_successful(atan2(y, x) * 180 / M_PI,
+                                atan2(z, sqrt(x * x + y * y)) * 180 / M_PI,
+                                success_rate_matrix)) {
         filtered_data.insert(filtered_data.end(), msg->data.begin() + offset,
                              msg->data.begin() + offset + point_step);
       }
+
       offset += point_step;
     }
 
